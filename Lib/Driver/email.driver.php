c977fbcf76ab283c0ec032eb91236a47c5d606ef
明月 <qiuyuexi@iisun.net>
2016-10-27 17:52:00 +0800

    修正邮件

--- Lib/Driver/email.driver.php
+++ Lib/Driver/email.driver.php
@@ -7,7 +7,7 @@ class email {
 		
 	private $mail;
 
-	public function __construct($send_address = 'xxx@qq.com',$title = 'PHPMailer发送邮件的示例',$body = '测试用例',$attach = array()){
+	public function __construct($send_address = 'xxxxx@qq.com'){
 		
 		//示例化PHPMailer核心类
 
@@ -33,38 +33,77 @@ class email {
 		$this->mail->CharSet = 'UTF-8';
 
 		//设置发件人姓名（昵称） 任意内容，显示在收件人邮件的发件人邮箱地址前的发件人姓名
-		$this->mail->FromName = 'xxx';
+		$this->mail->FromName = 'z8853797';
 
 		//smtp登录的账号 
-		$this->mail->Username ='xxx@163.com';
+		$this->mail->Username ='xxxx13896@163.com';
 
 		//smtp登录的密码 
-		$this->mail->Password = 'xxx';
+		$this->mail->Password = 'xxxxx';
 
 		//设置发件人邮箱地址 这里填入上述提到的“发件人邮箱”
-		$this->mail->From = 'xxxx';
+		$this->mail->From = '1xxxx@163.com';
 
 		//邮件正文是否为html编码 注意此处是一个方法 不再是属性 true或false
 		$this->mail->isHTML(true); 
 
+
+	}
+
+
+	public function setAttach($attach = array()){
+
 		//为该邮件添加附件 该方法也有两个参数 第一个参数为附件存放的目录（相对目录、或绝对目录均可） 第二参数为在邮件附件中该附件的名称
+		
 		foreach ($attach as $key => $value) {
+			
 			writeLog($value);
+			
 			$this->mail->addAttachment($value['path'],$value['name']);
 		
 		}
 
-		//设置收件人邮箱地址 该方法有两个参数 第一个参数为收件人邮箱地址 第二参数为给该地址设置的昵称 不同的邮箱系统会自动进行处理变动 
-
-		$this->mail->addAddress($send_address);
-
-		//添加该邮件的主题
-		$this->mail->Subject = $title;
-
-		//添加邮件正文 上方将isHTML设置成了true，则可以是完整的html字符串 如：使用file_get_contents函数读取本地的html文件
-		$this->mail->Body = $body;
-
 	}
+
+
+	/**
+	 * [setBody description]
+	 *@descripe
+	 *添加邮件正文 上方将isHTML设置成了true，则可以是完整的html字符串 如：使用file_get_contents函数读取本地的html文件 
+	 */
+	public function setBody($body){
+
+		$this->mail->Body = $body;
+	
+	}
+
+
+	/**
+	 * [setAddress description]
+	 * @param [type] $send_address [description]
+	 * @descripe 
+	 * //设置收件人邮箱地址 该方法有两个参数 第一个参数为收件人邮箱地址 第二参数为给该地址设置的昵称 不同的邮箱系统会自动进行处理变动 
+	 */
+	
+	public function setAddress($send_address = NULL){
+	
+		$this->mail->addAddress($send_address);
+	
+	}
+
+
+	/**
+	 * [setSubject description]
+	 * @param [type] $title [description]
+	 * 添加该邮件的主题
+	 */
+	public function setSubject($title = NULL){
+
+		$this->mail->Subject = $title;
+	}
+
+
+
 	/**
 	 * [send description]
 	 * @return [type] [description]
@@ -87,7 +126,22 @@ class email {
 
 		 	return 0;
 		 	writeLog($this->mail->ErrorInfo);
+
 		}
 	}
 
 } 
+
+/*
+	@example
+		$email = new email();
+		$email->setBody('http://www.baidu.com');
+		$email->setAddress('xxxx1@qq.com');
+		$email->setSubject('测试');
+		$email->setAttach(
+			array(
+				array('path'=>'/var/www/999.jpg','name'=>'img'),
+				)
+			);
+		$email->send();
+ */
